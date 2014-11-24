aa6ea4fbb29fbace35e3a6707e1dea757c149994
ahfergus1 <ahfergus1@gmail.com>
2014-11-24 16:49:42 -0500

    Updated table to second result as well.

--- asn3/TODO/getTAsByProf.php
+++ asn3/TODO/getTAsByProf.php
@@ -58,20 +58,39 @@
 			if(!$result){
 				die("databases query failed: " . mysqli_error($connection));
 			}
-			while($row=mysqli_fetch_assoc($result)){
-				$count++;
-				// Do sub-query for the specific TA
-				$query2='select * from TEACHINGASSISTANT where userid="' . $row["tauserid"] . '"';
-				$result2=mysqli_query($connection,$query2);
-				if(!$result2){
-					die("databases query failed: " . mysqli_error($connection));
+			if ($result->num_rows > 0){
+				while($row=mysqli_fetch_assoc($result)){
+					$query2='select * from TEACHINGASSISTANT where userid="' . $row["tauserid"] . '"';
+					$result2=mysqli_query($connection,$query2);
+					if(!$result2){
+						die("databases query failed: " . mysqli_error($connection));
+					}
+					// Only do if, should only be one result
+					if($row2=mysqli_fetch_assoc($result2)){
+						// Setup table
+						echo "<table id='display'>";
+						echo "<tr>
+								<td>User ID</td>
+								<td>First Name</td>
+								<td>Last Name</td>
+								<td>Grad Type</td>
+								<td>Profile Picture</td>
+							  </tr>";
+
+						// Print data into table.
+						while($row=mysqli_fetch_assoc($result)){
+							echo '<tr>';
+							echo "<td>{$row['userid']}</td>";
+							echo "<td>{$row['firstname']}</td>";
+							echo "<td>{$row['lastname']}</td>";
+							echo "<td>{$row['gradtype']}</td>";
+							echo '</tr>';
+							// TODO: Show image.
+						}
+						echo "</table>";
+					}
+					mysqli_free_result($result2);
 				}
-				if($row2=mysqli_fetch_assoc($result2)){
-					echo '<li>';
-					echo $row2["firstname"] . " " . $row2["lastname"] . " " . $row2["userid"] . " " . $row2["gradtype"] . "<br>";
-					// TODO: Show image.
-				}
-				mysqli_free_result($result2);
 			}
 			if ($result->num_rows == 0){
 				echo 'None';
