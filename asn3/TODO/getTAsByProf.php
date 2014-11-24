a388a8ea687c9cb1ea65df58f82790797f6b620e
ahfergus1 <ahfergus1@gmail.com>
2014-11-24 12:41:44 -0500

    Updated to also cover co-supervisors.

--- asn3/TODO/getTAsByProf.php
+++ asn3/TODO/getTAsByProf.php
@@ -12,6 +12,7 @@
 	<ol>
 		<?php
 			$wID= $_POST["profID"];
+			// Do head superviser seach:
 			$query = 'select * from ta where superID="' . $wID . '"';
 			$result=mysqli_query($connection,$query);
 			if(!$result){
@@ -19,12 +20,38 @@
 				echo "Query error: 	" . mysqli_error($connection);
 			}
 			echo "TA's that professor " . $fname . " " . $lname . " head supervises:<br>";
+			$count=0;
 			while($row=mysqli_fetch_assoc($result)){
+				$count++;
 				echo '<li>';
 				echo $row["fname"] . " " . $row["lname"] . " " . $row["westernID"] . " " . $row["type"] . "<br>";
-				//TODO: Show image.
+				// TODO: Show image.
 			}
 			mysqli_free_result($result);
+			$query = 'select * from ...';
+			if ($count == 0){
+				echo 'None';
+			}
+			echo '<br>';
+			// Do co-supervisor seach:
+			$query = 'select * from cosupervises where superID="' . $wID . '"';
+			$result=mysqli_query($connection,$query);
+			if(!$result){
+				die("databases query failed.");
+				echo "Query error: 	" . mysqli_error($connection);
+			}
+			echo "TA's that professor " . $fname . " " . $lname . " co-supervises:<br>";
+			$count=0;
+			while($row=mysqli_fetch_assoc($result)){
+				$count++;
+				echo '<li>';
+				echo $row["fname"] . " " . $row["lname"] . " " . $row["westernID"] . " " . $row["type"] . "<br>";
+				// TODO: Show image.
+			}
+			if ($count == 0){
+				echo 'None';
+			}
+			echo '<br>';
 		?>
 	</ol>
 	<?php
