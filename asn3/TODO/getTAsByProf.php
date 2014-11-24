4de8a68f9611383214803f52b0a45a02f35f8172
ahfergus1 <ahfergus1@gmail.com>
2014-11-24 16:57:26 -0500

    Removed ordered list stuff from table...

--- asn3/TODO/getTAsByProf.php
+++ asn3/TODO/getTAsByProf.php
@@ -10,91 +10,87 @@
 	?>
 	<h1>TA's Supervised by Professor </h1>
 	<h2>TA's that professor head supervises:</h2>
-	<ol>
-		<?php
-			$wID= $_POST["profID"];
-			// Do head superviser seach:
-			$query = 'select * from TEACHINGASSISTANT where profuserid="' . $wID . '"';
-			$result=mysqli_query($connection,$query);
-			if(!$result){
-				die("databases query failed.");
-				echo "Query error: 	" . mysqli_error($connection);
-			}
-			if ($result->num_rows > 0){
-				// Setup table
-				echo "<table id='display'>";
-				echo "<tr>
-						<td>User ID</td>
-						<td>First Name</td>
-						<td>Last Name</td>
-						<td>Grad Type</td>
-						<td>Profile Picture</td>
-					  </tr>";
+	<?php
+		$wID= $_POST["profID"];
+		// Do head superviser seach:
+		$query = 'select * from TEACHINGASSISTANT where profuserid="' . $wID . '"';
+		$result=mysqli_query($connection,$query);
+		if(!$result){
+			die("databases query failed.");
+			echo "Query error: 	" . mysqli_error($connection);
+		}
+		if ($result->num_rows > 0){
+			// Setup table
+			echo "<table id='display'>";
+			echo "<tr>
+					<td>User ID</td>
+					<td>First Name</td>
+					<td>Last Name</td>
+					<td>Grad Type</td>
+					<td>Profile Picture</td>
+				  </tr>";
 
-				// Print data into table.
-				while($row=mysqli_fetch_assoc($result)){
+			// Print data into table.
+			while($row=mysqli_fetch_assoc($result)){
+				echo '<tr>';
+				echo "<td>{$row['userid']}</td>";
+				echo "<td>{$row['firstname']}</td>";
+				echo "<td>{$row['lastname']}</td>";
+				echo "<td>{$row['gradtype']}</td>";
+				echo '</tr>';
+				// TODO: Show image.
+			}
+			echo "</table>";
+		}
+		if ($result->num_rows == 0){
+			echo 'None';
+		}
+		mysqli_free_result($result);
+	?>
+	<h2>TA's that professor co-supervises:</h2>
+	<?php
+		// Do co-supervisor seach:
+		$query = 'select * from CoSUPERVISE where profuserid="' . $wID . '"';
+		$result=mysqli_query($connection,$query);
+		if(!$result){
+			die("databases query failed: " . mysqli_error($connection));
+		}
+		if ($result->num_rows > 0){
+			// Setup table
+			echo "<table id='display'>";
+			echo "<tr>
+					<td>User ID</td>
+					<td>First Name</td>
+					<td>Last Name</td>
+					<td>Grad Type</td>
+					<td>Profile Picture</td>
+				  </tr>";
+			while($row=mysqli_fetch_assoc($result)){
+				$query2='select * from TEACHINGASSISTANT where userid="' . $row["tauserid"] . '"';
+				$result2=mysqli_query($connection,$query2);
+				if(!$result2){
+					die("databases query failed: " . mysqli_error($connection));
+				}
+				// Only do if, should only be one result
+				if($row2=mysqli_fetch_assoc($result2)){
+					// Print data into table.
 					echo '<tr>';
-					echo "<td>{$row['userid']}</td>";
-					echo "<td>{$row['firstname']}</td>";
-					echo "<td>{$row['lastname']}</td>";
-					echo "<td>{$row['gradtype']}</td>";
+					echo "<td>{$row2['userid']}</td>";
+					echo "<td>{$row2['firstname']}</td>";
+					echo "<td>{$row2['lastname']}</td>";
+					echo "<td>{$row2['gradtype']}</td>";
 					echo '</tr>';
 					// TODO: Show image.
 				}
-				echo "</table>";
+				mysqli_free_result($result2);
 			}
-			if ($result->num_rows == 0){
-				echo 'None';
-			}
-			mysqli_free_result($result);
-		?>
-	</ol>
-	<h2>TA's that professor co-supervises:</h2>
-	<ol>
-		<?php
-			// Do co-supervisor seach:
-			$query = 'select * from CoSUPERVISE where profuserid="' . $wID . '"';
-			$result=mysqli_query($connection,$query);
-			if(!$result){
-				die("databases query failed: " . mysqli_error($connection));
-			}
-			if ($result->num_rows > 0){
-				// Setup table
-				echo "<table id='display'>";
-				echo "<tr>
-						<td>User ID</td>
-						<td>First Name</td>
-						<td>Last Name</td>
-						<td>Grad Type</td>
-						<td>Profile Picture</td>
-					  </tr>";
-				while($row=mysqli_fetch_assoc($result)){
-					$query2='select * from TEACHINGASSISTANT where userid="' . $row["tauserid"] . '"';
-					$result2=mysqli_query($connection,$query2);
-					if(!$result2){
-						die("databases query failed: " . mysqli_error($connection));
-					}
-					// Only do if, should only be one result
-					if($row2=mysqli_fetch_assoc($result2)){
-						// Print data into table.
-						echo '<tr>';
-						echo "<td>{$row2['userid']}</td>";
-						echo "<td>{$row2['firstname']}</td>";
-						echo "<td>{$row2['lastname']}</td>";
-						echo "<td>{$row2['gradtype']}</td>";
-						echo '</tr>';
-						// TODO: Show image.
-					}
-					mysqli_free_result($result2);
-				}
-				echo "</table>";
-			}
-			if ($result->num_rows == 0){
-				echo 'None';
-			}
-			mysqli_free_result($result);
-		?>
-	</ol>
+			echo "</table>";
+		}
+		if ($result->num_rows == 0){
+			echo 'None';
+		}
+		mysqli_free_result($result);
+	?>
 	<?php
     	mysqli_close($connection);
   	?>
