3655affdb14ecb6c81365a305275bd9228f91b91
ahfergus1 <ahfergus1@gmail.com>
2014-11-24 16:53:38 -0500

    Fixed second table...

--- asn3/TODO/getTAsByProf.php
+++ asn3/TODO/getTAsByProf.php
@@ -59,6 +59,15 @@
 				die("databases query failed: " . mysqli_error($connection));
 			}
 			if ($result->num_rows > 0){
+				// Setup table
+				echo "<table id='display'>";
+				echo "<tr>
+						<td>User ID</td>
+						<td>First Name</td>
+						<td>Last Name</td>
+						<td>Grad Type</td>
+						<td>Profile Picture</td>
+					  </tr>";
 				while($row=mysqli_fetch_assoc($result)){
 					$query2='select * from TEACHINGASSISTANT where userid="' . $row["tauserid"] . '"';
 					$result2=mysqli_query($connection,$query2);
@@ -67,30 +76,18 @@
 					}
 					// Only do if, should only be one result
 					if($row2=mysqli_fetch_assoc($result2)){
-						// Setup table
-						echo "<table id='display'>";
-						echo "<tr>
-								<td>User ID</td>
-								<td>First Name</td>
-								<td>Last Name</td>
-								<td>Grad Type</td>
-								<td>Profile Picture</td>
-							  </tr>";
-
 						// Print data into table.
-						while($row=mysqli_fetch_assoc($result)){
-							echo '<tr>';
-							echo "<td>{$row['userid']}</td>";
-							echo "<td>{$row['firstname']}</td>";
-							echo "<td>{$row['lastname']}</td>";
-							echo "<td>{$row['gradtype']}</td>";
-							echo '</tr>';
-							// TODO: Show image.
-						}
-						echo "</table>";
+						echo '<tr>';
+						echo "<td>{$row2['userid']}</td>";
+						echo "<td>{$row2['firstname']}</td>";
+						echo "<td>{$row2['lastname']}</td>";
+						echo "<td>{$row2['gradtype']}</td>";
+						echo '</tr>';
+						// TODO: Show image.
 					}
 					mysqli_free_result($result2);
 				}
+				echo "</table>";
 			}
 			if ($result->num_rows == 0){
 				echo 'None';
