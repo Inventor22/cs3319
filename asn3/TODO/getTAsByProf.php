e7c64016664f7f17a8d6637971b27e24cfe16fc3
ahfergus1 <ahfergus1@gmail.com>
2014-11-24 02:00:17 -0500

    Was using the wrong quote type...

--- asn3/TODO/getTAsByProf.php
+++ asn3/TODO/getTAsByProf.php
@@ -12,7 +12,7 @@
 	<ol>
 		<?php
 			$wID= $_POST["profID"];
-			$query = "select westernID from prof where westernID=" . $wID . '"';
+			$query = 'select westernID from prof where westernID="' . $wID . '"';
 			$result=mysqli_query($connection,$query);
 			if($result){
 				die("databases query failed.");
