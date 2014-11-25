bea9ab794eccff401538352a8fb73dd2cf195c05
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2014-11-24 20:35:51 -0500

    da

--- asn3/Finished/ModifyTA.php
+++ asn3/Finished/ModifyTA.php
@@ -28,19 +28,16 @@
     $from_studentnumber = $_POST["studentnumber"];
     
     $to_firstname = $_POST["newfirstname"];
-    $to_lastname  = $_POST["newlastname"]; 
-    
-    echo $to_firstname;
-    echo $to_lastname;
+    $to_lastname  = $_POST["newlastname"];
     
     if (strlen($from_tauserid) > 0) {
         $changeTA = "update TEACHINGASSISTANT 
-                    set lastname='".$to_lastname."', firstname='".$to_firstname."'".
-                    " where userid='".$from_tauserid."'";
+                    set lastname='".$to_lastname."', firstname='".$to_firstname."'"
+                    ." where userid='".$from_tauserid."'";
     } else if (strlen($from_firstname) > 0 && strlen($from_lastname) > 0) {
         $changeTA = "UDPATE TEACHINGASSISTANT 
-                    set lastname='$to_lastname', firstname='$to_firstname'
-                    where firstname='$from_firstname' AND lastname='$from_lastname')";
+                    set lastname='".$to_lastname."', firstname='".$to_firstname."'"
+                    ." where firstname='".$from_firstname."' AND lastname='".$from_lastname."')";
     } else if (strlen($from_studentnumber) > 0) {
         $changeTA = "UDPATE TEACHINGASSISTANT 
                     set lastname='$to_lastname', firstname='$to_firstname'
