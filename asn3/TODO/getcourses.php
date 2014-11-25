45ad9c6186684c22a9016f4b3fd09d82d64fc06d
ahfergus1 <ahfergus1@gmail.com>
2014-11-25 13:05:31 -0500

    Fixed issue with last commit.

--- asn3/TODO/getcourses.php
+++ asn3/TODO/getcourses.php
@@ -6,7 +6,7 @@
     }
     while($row = mysqli_fetch_assoc($result)){
         echo '<input type="radio" name="c_code" value="';
-        echo $row["userid"];
+        echo $row["coursenumber"];
         echo '">' . $row["coursenumber"] . " - " . $row["coursename"] . "<br>";
     }
     mysqli_free_result($result);
