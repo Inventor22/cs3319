4a8aa5d56cabd7713f6995c1349091f2e70693ba
ahfergus1 <ahfergus1@gmail.com>
2014-11-26 01:11:42 -0500

    Image detection test.

--- asn3/Finished/getTAsByProf.php
+++ asn3/Finished/getTAsByProf.php
@@ -56,7 +56,8 @@
 					echo "<td><img src={$row['imagelocation']} height=\"60\" width=\"60\"></td>";
 				}
 				else {
-					echo "<td><img src=TA_Pictures/default0.jpg height=\"60\" width=\"60\"></td>";
+					echo "<td>No image.</td>";
+					//echo "<td><img src=TA_Pictures/default0.jpg height=\"60\" width=\"60\"></td>";
 				}
 				echo '</tr>';
 			}
@@ -103,7 +104,8 @@
 						echo "<td><img src={$row2['imagelocation']} height=\"60\" width=\"60\"></td>";
 					}
 					else {
-						echo "<td><img src=TA_Pictures/default0.jpg height=\"60\" width=\"60\"></td>";
+						echo "<td>No image.</td>";
+						//echo "<td><img src=TA_Pictures/default0.jpg height=\"60\" width=\"60\"></td>";
 					}
 					echo '</tr>';
 				}
