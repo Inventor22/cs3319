2a8ea5c04e215e6fb744bec59306b25b69c625ca
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2014-11-26 02:06:32 -0500

    cs

--- asn3/Finished/ProfToTA.php
+++ asn3/Finished/ProfToTA.php
@@ -54,6 +54,9 @@
             $final_profid = mysqli_fetch_assoc($getprofid_result)["userid"];
             $final_taid   = mysqli_fetch_assoc($gettaid_result)["userid"];
             
+            echo '<br>'.$final_profid;
+            echo '<br>'.$final_taid;
+            
             if($_POST['submit']==0) // Assign Prof as Head Supervisor for TA
             {
                 $assign =
