0a580aff936355ccf9caf2cd1eb791dc8ed7ff09
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2014-11-28 13:15:15 -0500

    gbtrfe

--- asn3/Finished/AddNewTA.php
+++ asn3/Finished/AddNewTA.php
@@ -82,29 +82,27 @@
                     </tr>
                     <tr>
                         <td>userid:</td>
-                        <td>$_POST['userid']; ?></td>
+                        <td>$_POST['userid']</td>
                     </tr>
                     <tr>
                         <td>firstname:</td>
-                        <td>
-                            $_POST['firstname']; ?></td>
+                        <td>$_POST['firstname']</td>
                     </tr>
                     <tr>
                         <td>lastname:</td>
-                        <td>
-                            $_POST['lastname']; ?></td>
+                        <td>$_POST['lastname']</td>
                     </tr>
                     <tr>
                         <td>studentnumber:</td>
-                        <td> $_POST['studentnumber']; ?></td>
+                        <td>$_POST['studentnumber']</td>
                     </tr>
                     <tr>
                         <td>gradtype:</td>
-                        <td> $_POST['type'] </td>
+                        <td>$_POST['type'] </td>
                     </tr>
                     <tr>
                         <td>Head Prof:</td>
-                        <td> $_POST['profuserid'] </td>
+                        <td>$_POST['profuserid'] </td>
                     </tr>
                   </table>";
         }
